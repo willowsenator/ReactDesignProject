0f97bcc972057896184aed5719e2321b22845db9
willowsenator <willowsenator@gmail.com>
2023-10-23 22:47:43 +0200

    Add PriceCards and keys for components

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,5 +1,26 @@
+import data from "../resources/data.json"
+function Section({item}){
+
+    return <div>
+      <h5>{item.name}</h5>
+      <ul className="nav flex-column">
+        {item.links.map((item, index) => <li key={`${item.name}_${index}`}>
+
+            <a target="_blank" className="text-decoration-none" href={item.url}>{item.text}</a>
+        </li>)}
+      </ul>
+    </div>;
+}
 export function Footer(){
-    return (<div>
-        <h1>Footer</h1>
+    return (<div className="mx-3 d-flex mt-4 justify-content-between">
+        <div className="fs-4">
+            {data.header.name}
+        </div>
+        {
+            data.footer.map((item, index) => <Section key={`${item.name}_${index}`} item={item}>
+
+            </Section>
+           )
+        }
     </div>);
 }
--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -3,14 +3,14 @@ import data from "../resources/data.json";
 
 export function Header(){
     return (<div className="my-2 text-dark d-flex justify-content-between">
-        <div className="d-flex">
+        <div className="d-flex mx-3">
             <Logo></Logo>
-            <p className="fs-1">{data.header.name}</p>
+            <p className="fs-4">{data.header.name}</p>
         </div>
         <div>
             {
                 data.header.links.map((item, index) => 
-                <a key={index} href={item.url} className="mx-3 text-decoration-none">{item.text}</a>)
+                <a key={index} target="_blank" href={item.url} className="mx-3 text-decoration-none">{item.text}</a>)
             }
         </div>
     </div>);
